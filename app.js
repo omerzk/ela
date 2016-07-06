09975a0061c73476c009e40c1f541db41dfbc6c1
omerzk <omeryom@gmail.com>
2016-07-06 13:13:15 +0300

    cleanup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use strict";
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -10,9 +9,6 @@ var sf = require('slice-file');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 let logFile ="./requests.log" //"~/ela/logs/requests.log";
 let textFile = "./textFile"//"~/ela/textFile";
 let sizeLog = "./size.log"//"~/ela/logs/size.log";
